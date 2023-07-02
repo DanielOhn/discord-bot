ab3e97c34315879b98a54a12f13f32cbbfaf63af
DanielOhn <ohndaniel@gmail.com>
2023-07-01 19:34:33 -0700

    Add and update addContent.js

--- commands/content/addContent.js
+++ commands/content/addContent.js
@@ -6,28 +6,34 @@ const addContent = {
     data: new SlashCommandBuilder()
         .setName('add-content')
         .setDescription('Add a movie or a show to the database.')
-        .addStringOption(option => 
+        .addStringOption(option =>
             option.setName("title")
-            .setDescription("Title of the movie or show")
-            .setRequired(true)    
-        ),
+                .setDescription("Title of the movie or show")
+                .setRequired(true),
+        )
+        .addStringOption(option =>
+            option.setName("type")
+                .setDescription("Set if it's a movie or show. Defaults as a movie.")
+                .addChoices(
+                    { name: "Movie", value: "Movie" },
+                    { name: "Show", value: "Show" },
+                )
+            ),
     async execute(interaction) {
+        const getName = interaction.options.data[0].value
+        const getType = interaction.options.data[1].value
 
-        //const name = interaction.options.getString("name");
+        try {
+            const content = await Content(sequelize).create({
+                name: getName,
+                media: getType
+            });
 
-        const msg = `${interaction}`;
-        console.log(msg.split(":")[1]);
-
-        // try {
-        //     const content = await Content(sequelize).create({
-        //         name: name
-        //     });
-
-        //     return interaction.reply(`${name} has been added.`);
-        // } catch (err) {
-        //     console.log("Couldn't add it to the db, shits fucked yo! ", err);
-        // }
-	},
+            return interaction.reply(`#${content.dataValues.id} - ${getName} has been added.`);
+        } catch (err) {
+            console.log("Couldn't add it to the db, shits fucked yo! ", err);
+        }
+    },
 };
 
 
